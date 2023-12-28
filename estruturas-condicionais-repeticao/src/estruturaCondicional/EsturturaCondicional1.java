076372a0037f974684d69259cf2d7449f3b4a205
Willian Pires Sabadini Zuqui <wpszuqui@gmail.com>
2023-12-28 16:32:00 -0300

    nova versão

--- estruturas-condicionais-repeticao/src/estruturaCondicional/EsturturaCondicional1.java
+++ estruturas-condicionais-repeticao/src/estruturaCondicional/EsturturaCondicional1.java
@@ -3,7 +3,15 @@ package estruturaCondicional;
 public class EsturturaCondicional1 {
 
 	public static void main(String[] args) {
-		int nota1;
+		int mediaAluno = 70;
+		String nome = "Fabio";
+		if (mediaAluno >= 70 && nome.equals("Alex")) {
+			System.out.println("Parabens! Voce esta aprovado");
+		} else if (mediaAluno < 70) {
+			System.out.println("Voce esta reprovado");
+		} else {
+			System.out.println("Aluno nao encontrado");
+		};
 		
 
 	}
